7ec65a0b2ed375f82f586c5ed82c077ef4a3af7c
Tran Hung <tranqhung36@gmail.com>
2021-12-30 17:36:04 +0700

    fixed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const siteRoute = require('./home.route')
+const userRoute = require('./user.route')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/user', userRoute)
+router.use('/', siteRoute)
 
-module.exports = router;
+module.exports = router

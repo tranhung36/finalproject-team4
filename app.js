6cb5e2aa38c1e75de5a4f2a62040859ea1f2c246
Tran Hung <tranqhung36@gmail.com>
2021-12-31 11:01:04 +0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const createError = require('http-errors');
 const express = require('express');
+const morgan = require('morgan')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const expressLayouts = require('express-ejs-layouts')
 const db = require('./config/database/db')
 
 const routes = require('./routes');
@@ -16,8 +18,11 @@ db.connect()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layouts/layout')
 
+app.use(expressLayouts)
 app.use(logger('dev'));
+app.use(morgan('combined'))
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const siteRoute = require('./home.route')
 const userRoute = require('./user.route')
+const productRoute = require('./product.route')
 
+router.use('/product', productRoute)
 router.use('/user', userRoute)
 router.use('/', siteRoute)
 

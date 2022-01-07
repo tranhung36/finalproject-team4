8ded833ab9675b42380a2051a7cc5d38902dafa8
Tran Hung <tranqhung36@gmail.com>
2022-01-07 14:49:00 +0700

    fixed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -33,8 +33,8 @@ async function register(req, res) {
 
     //Create user in database
     const user = await User.create({
-      first_name,
-      last_name,
+      first_name: first_name.trim(),
+      last_name: last_name.trim(),
       email: email.toLowerCase(),
       password: encryptedPassword,
     });

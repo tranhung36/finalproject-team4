a25c88bc8362dc6abbf7a329bf444f7d145997df
p.nguyen <phuocnguyen24598@gmail.com>
2022-01-11 11:19:39 +0700

    handle delete btn

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -8,27 +8,29 @@ const loadValue = async () => {
         const productString = searchStringValue.product
         const abc = productString
         searchInput.oninput = (e) => {
-            const searchInputValue = e.target.value
+            const searchInputValue = e.target.value.toLowerCase()
             const filterSearchValue = abc.filter(value => {
-                if(searchInputValue === ''){
+                if (searchInputValue === '') {
                     return ''
-                }else{
-                    return value.name.includes(searchInputValue)
+                } else {
+                    return value.name.toLowerCase().includes(searchInputValue) ||
+                        value.slug.toLowerCase().includes(searchInputValue)
                 }
             })
             if (filterSearchValue != []) {
                 const htmlString = filterSearchValue.slice(0, 10).map(item => {
+                    const price = `${item.price.toLocaleString()}`
                     return `<li class="search-item">
                                 <a href="${item._id}">
                                     <img src= "./../../images/${item.thumbnail}" alt="product Image" >
                                     <div class="item-info">
                                         <p>${item.name}</p>
-                                        <p>${item.price}</p>
+                                        <p>${price}đ</p>
                                     </div>
                                 </a>
                             </li>`
                 })
-                
+
                 searchList.innerHTML = htmlString.join(' ')
             } else {
                 const htmlStringg = `<li class="searchItem"></li>`
@@ -40,3 +42,15 @@ const loadValue = async () => {
     }
 }
 loadValue();
+const btnDeleteProduct = document.querySelector('.delete-product')
+
+// CRUD-page handle delete Button
+btnDeleteProduct.onclick = (e) => {
+    const result = confirm("Bạn có muốn xóa không?");
+    if (result) {
+        alert("Xóa thành công");
+    } else {
+        e.preventDefault()
+    }
+    
+}

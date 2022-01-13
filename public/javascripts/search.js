e33a04de6e8d1617bc4d614ae3de5aac2d7b5c72
p.nguyen <phuocnguyen24598@gmail.com>
2022-01-13 20:12:20 +0700

    completed search pagination except 1 bug :)))

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,6 +1,8 @@
 const searchList = document.querySelector('#search-list')
 const searchInput = document.querySelector('#search-input')
-
+const serverPages = document.querySelector('.server-pg')
+const clientPage = document.querySelector('.client-pg')
+clientPage.style.display = 'none'
 const loadValue = async () => {
     try {
         // call api
@@ -39,50 +41,65 @@ const loadSearchValue = async (key) => {
     const searchValue = await fetch(`http://localhost:8080/search/searchApi?key=${key}`)
     const searchStringValue = await searchValue.json()
     const searchData = searchStringValue.data
-    // console.log(searchData);
-    const selection = document.querySelector('.sp-sort-by')
-    selection.onclick = (e) => {
+    const selections = document.querySelector('.sp-sort-by')
+    selections.onclick = (e) => {
         if (e.target.value == 'priceAsc') {
-            const sort = searchData.sort((a, b) => a.price < b.price ? -1 : 1)
-            sortProduct(sort)
-        }if(e.target.value == 'priceDesc'){
-            const sort = searchData.sort((a, b) => a.price > b.price ? -1 : 1)
-            sortProduct(sort)
-        }if(e.target.value == 'dateDesc'){
-            const sort = searchData.sort((a, b) => a.updatedAt > b.updatedAt ? -1 : 1)
-            sortProduct(sort)
-        }else{
+            const sort = searchData.sort((a, b) => a.price > b.price ? 1 : -1)
+            sortProduct(sort, searchData)
+        } if (e.target.value == 'priceDesc') {
+            const sort = searchData.sort((a, b) => a.price < b.price ? 1 : -1)
+            sortProduct(sort, searchData)
+        } if (e.target.value == 'dateDesc') {
+            const sort = searchData.sort((a, b) => a.updatedAt < b.updatedAt ? 1 : -1)
+            sortProduct(sort, searchData)
+        } else {
             return 0
         }
     }
 }
 
-const sortProduct = (sort) =>{
-    const ascString = sort.map(s =>{
-        return `
-        <div class="col-xl-3 col-lg-4 col-sm-6">
-            <div class="product text-center">
-              <div class="position-relative mb-3">
-                <div class="badge text-white bg-"></div><a class="d-block" href="detail.html"><img class="img-fluid w-100"
-                    src="/../images/${s.thumbnail}" alt="..."></a>
-                <div class="product-overlay">
-                  <ul class="mb-0 list-inline">
-                    <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#!"><i
-                          class="far fa-heart"></i></a></li>
-                    <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark" href="/cart">Add to cart</a>
-                    </li>
-                    <li class="list-inline-item me-0"><a class="btn btn-sm btn-outline-dark" href="#productView"
-                        data-bs-toggle="modal"><i class="fas fa-expand"></i></a></li>
-                  </ul>
+const sortProduct = (sort, searchData) => {
+    serverPages.style.display = 'none'
+    clientPage.style.display = 'block'
+    const page = document.querySelector('.client-pg ul')
+    const pages = Math.ceil(searchData.length / 8)
+    let html = ``
+    for (let i = 0; i < pages; i++) {
+        html += `<li class="page-item"><a class="page-link"</a>${i + 1}</li>`
+    }
+    page.innerHTML = html
+    const pageValue = document.querySelectorAll('.client-pg ul li a')
+    pageValue.forEach(page => {
+        page.onclick = () => {
+            let value =  Number(page.innerText) 
+            const ascString = sort.slice(8 * (value - 1), 8 * (value)).map(s => {
+                return `
+                <div class="col-xl-3 col-lg-4 col-sm-6">
+                    <div class="product text-center">
+                      <div class="position-relative mb-3">
+                        <div class="badge text-white bg-"></div><a class="d-block" href="detail.html"><img class="img-fluid w-100"
+                            src="/../images/${s.thumbnail}" alt="..."></a>
+                        <div class="product-overlay">
+                          <ul class="mb-0 list-inline">
+                            <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#!"><i
+                                  class="far fa-heart"></i></a></li>
+                            <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark" href="/cart">Add to cart</a>
+                            </li>
+                            <li class="list-inline-item me-0"><a class="btn btn-sm btn-outline-dark" href="#productView"
+                                data-bs-toggle="modal"><i class="fas fa-expand"></i></a></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <h6> <a class="reset-anchor" href="detail.html">${s.name}</a></h6>
+                      <p class="small text-muted">${s.price}</p>
+                    </div>
                 </div>
-              </div>
-              <h6> <a class="reset-anchor" href="detail.html">${s.name}</a></h6>
-              <p class="small text-muted">${s.price}</p>
-            </div>
-        </div>
-        `
+                `
+            })
+            document.querySelector('.sp-right .row ').innerHTML = ascString.join(' ')
+        }
     })
-    document.querySelector('.sp-right .row ').innerHTML = ascString.join(' ')
+
 }
 
 loadSearchValue(document.querySelector('.key').innerText)

6da21e0ab9810550d04f7a968583f3ef0de32b17
Harry Nguyen <nguyenquanghuy2021@gmail.com>
2022-01-13 09:04:07 +0700

    update sort

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,55 +1,58 @@
-const Product = require('../models/product.model')
+const Product = require("../models/product.model");
+const Category = require("../models/category.model");
 
 async function index(req, res, next) {
-    try {
-        let perPage = 12; // số lượng sản phẩm xuất hiện trên 1 page
-        let page = req.query.page || 1;
+  try {
+    let perPage = 12; // số lượng sản phẩm xuất hiện trên 1 page
+    let page = req.query.page || 1;
+    let sortType = req.query.show;
 
-        let products = await Product.find()
-            .skip(perPage * page - perPage)
-            .limit(perPage)
-            .sort("-price");
+    let sortBy = sortType=== "priceAsc" ? "price" : sortType=== "priceDesc" ? "-price" : ""
+    let products = await Product.find()
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .sort(sortBy)
 
-        let count = await Product.countDocuments();
+    let count = await Product.countDocuments();
 
-        res.render('products/shop', {
-            products, // sản phẩm trên một page
-            current: page, // page hiện tại
-            pages: Math.ceil(count / perPage), // tổng số các page
-            count: count // tổng sản phẩm
-        });
-    } catch (err) {
-        return res.status(500).json({
-            msg: err.message
-        });
-    }
+    let categories = await Category.find();
+    res.render("products/shop.ejs", {
+      products, // sản phẩm trên một page
+      current: page, // page hiện tại
+      pages: Math.ceil(count / perPage), // tổng số các page
+      count: count, // tổng sản phẩm
+      categories: categories, // loại danh mục
+    });
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
   }
-  
-
+}
 
 async function show(req, res, next) {
-    try {
-        const product = await Product.findOne({
-            slug: req.params.slug
-        })
+  try {
+    const product = await Product.findOne({
+      slug: req.params.slug,
+    });
 
-        if (!product) {
-            return res.render('error', {
-                message: 'Product not found',
-                title: 'Not Found'
-            })
-        }
-
-        res.render('products/detail', {
-            title: 'Product',
-            product
-        })
-    } catch (error) {
-        next(error)
+    if (!product) {
+      return res.render("error", {
+        message: "Product not found",
+        title: "Not Found",
+      });
     }
+
+    res.render("products/detail", {
+      title: "Product",
+      product,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
-    index,
-    show,
-}
+  index,
+  show,
+};
--- routes/product.route.js
+++ routes/product.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {
     index,
-    show
+    show,
 } = require('../controllers/product.controller')
 
 /* GET product page. */

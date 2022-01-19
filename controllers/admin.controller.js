c7c43f7d14bd0ab589d0badc5fc805add11717cc
Harry Nguyen <nguyenquanghuy2021@gmail.com>
2022-01-19 17:15:53 +0700

    crud manage user

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,9 +1,10 @@
 const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
 
-async function redenerManageUser(req, res) {
+async function renderManageUserPage(req, res) {
   try {
     let userInfo = await User.find();
-    
+
     res.render("admin/ManageUserPage", {
       userInfo,
     });
@@ -13,7 +14,86 @@ async function redenerManageUser(req, res) {
     });
   }
 }
+async function renderCreateUserPage(req, res) {
+  res.render("admin/createUserPage");
+  ``;
+}
+async function createUser(req, res) {
+  try {
+    let email = req.body.email;
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    let password = req.body.password;
+    //Encrypt user password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      first_name: firstname,
+      last_name: lastname,
+      email: email,
+      password: encryptedPassword,
+    });
+    await user.save();
+    res.redirect("/admin/manageUsers");
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+}
+async function renderUpdateUserPage(req, res) {
+  try {
+    let id = req.params.id;
+    const user = await User.findById({
+      _id: id,
+    });
+    //res.json(user)
+    res.render("admin/updateUserPage", { user });
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+}
+async function updateUser(req, res) {
+  try {
+    let id = req.params.id;
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    await User.findOneAndUpdate(
+      {
+        _id: id
+      },
+      {
+        first_name: firstname,
+        last_name: lastname,
+      }
+    );
+    res.redirect("/admin/manageUsers");
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+}
+async function deleteUser(req, res) {
+  try {
+    let id = req.params.id;
+    await User.findByIdAndDelete({
+      _id: id,
+    });
+    res.redirect("/admin/manageUsers");
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+}
 
 module.exports = {
-    redenerManageUser,
-  };
+  renderManageUserPage,
+  renderUpdateUserPage,
+  renderCreateUserPage,
+  deleteUser,
+  createUser,
+  updateUser,
+};
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { redenerManageUser } = require("../controllers/admin.controller");
+const {
+  renderManageUserPage,
+  renderUpdateUserPage,
+  renderCreateUserPage,
+  deleteUser,
+  createUser,
+  updateUser,
+} = require("../controllers/admin.controller");
 
-router.get("/", redenerManageUser);
+router.get("/", renderManageUserPage);
+router.get("/manageUsers", renderManageUserPage);
+
+router.get("/manageUsers/createUser", renderCreateUserPage);
+router.post("/manageUsers/createUser", createUser);
+
+router.get("/manageUsers/updateUser/:id", renderUpdateUserPage);
+router.put("/manageUsers/updateUser/:id", updateUser);
+
+router.get("/manageUsers/deleteUser/:id", deleteUser);
 
 module.exports = router;
